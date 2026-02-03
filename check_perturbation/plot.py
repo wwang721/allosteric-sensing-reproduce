6455faafb902d9c7ee379969096d2b8d90022f82
Wei Wang <ww000721@gmail.com>
2026-02-03 14:45:06 +0800

    :bento: Add code & data for Fig. 5

--- check_perturbation/plot.py
+++ check_perturbation/plot.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+data = np.load('FI_calc.npy')
+FI_calc_highdiff = data[0]
+FI_calc_lowdiff = data[1]
+
+
 max_steps = 600000
 time = [0]
 
@@ -34,11 +40,9 @@ r = 1  # position in units of r
 dt = 0.00001
 dx = 2*np.pi*r/l
 D = 100  # units of x^2/t
-# D*dt/dx**2  # checking stability - this has to be < 0.5
-
 
 C_list1 = np.logspace(0, 3, 100)
-
+C_list = np.logspace(0, 2.5, 12)
 D_h = 50
 D_l = 0.1
 FI_high_pert = n * g**2 / 32 * \
@@ -65,20 +69,49 @@ FI_two_rec = n * g**2 * C_list1 * KD / \
     (C_list1 + KD)**2 / 16 + n * g**2 * C_list1 * \
     KD/alpha / (C_list1 + KD/alpha)**2 / 16
 
+perfect_adap_index = np.logical_and(C_list1 >= KD/alpha, C_list1 <= KD)
 
-fig, ax = plt.subplots(1, 1, dpi=100, figsize=(4, 3))
+
+# --------------------------------------------------------------------
+
+fig, ax = plt.subplots(1, 1, figsize=(3.5, 3))
 
 ax.set_xscale('log')
 
-ax.plot(C_list1, FI_perfect, '-', color="C2", label=r"Adjust $K_\mathrm{eff}$ to $C_0$", lw=2, zorder=1)
-ax.plot(C_list1, FI_single_rec, '--', color='C3', label="Single receptor type", lw=2, zorder=2)
-ax.plot(C_list1, FI_two_rec, '-.', color="C0", label="Two receptor types", lw=2, zorder=1)
+ax.plot(C_list1, FI_perfect, '-', color="C2", label=r"Optimal", lw=2, zorder=1)
+ax.plot(C_list, FI_calc_highdiff, 'o', color="C0", label="Simulations", markerfacecolor="None", markeredgewidth=1.5, zorder=3, clip_on=False)
+ax.plot(C_list1[perfect_adap_index], FI_high_pert[perfect_adap_index], '--', lw=2.2, color="C3", label="Perturbation", zorder=2)
+
 ax.axvspan(KD/alpha, KD, facecolor='C2', alpha=0.1, edgecolor='none', zorder=0)
 
-ax.set_xlabel(r"Mean ligand concentration $C_0$ [nM]")
-ax.set_ylabel(r"Optimal Fisher information $I_{\phi\phi}$")
+ax.set_xlabel(r"Mean concentration $C_0$ [nM]")
+ax.set_ylabel(r"Fisher information $I_{\phi\phi}$")
 ax.set_xlim(1, 1000)
 ax.set_ylim(0, 34)
 ax.set_yticks([5, 15, 25], minor=True)
-plt.legend(frameon=False, fontsize=11)
-plt.savefig('F_comparing.png', dpi=150, bbox_inches='tight')
+plt.title(r"$\!\tilde{\,D}=50$")
+plt.legend(frameon=False, fontsize=13, loc="lower right")
+plt.savefig('high_D.png', dpi=150, bbox_inches='tight')
+
+
+#--------------------------------------------------------------------
+
+fig, ax = plt.subplots(1, 1, figsize=(3.5, 3))
+
+ax.set_xscale('log')
+ax.set_yscale('log')
+
+ax.plot(C_list1, FI_perfect, '-', color="C2", label=r"Optimal", lw=2, zorder=1)
+ax.plot(C_list, FI_calc_lowdiff, 'o', color="C0", label="Simulations", markerfacecolor="None", markeredgewidth=1.5, zorder=3, clip_on=False)
+ax.plot(C_list1[perfect_adap_index], FI_low_pert[perfect_adap_index], '--', lw=2.2, color="C3", label="Perturbation", zorder=2)
+
+ax.axvspan(KD/alpha, KD, facecolor='C2', alpha=0.1, edgecolor='none', zorder=0)
+
+
+ax.set_xlabel(r"Mean concentration $C_0$ [nM]")
+ax.set_ylabel(r"Fisher information $I_{\phi\phi}$")
+ax.set_xlim(1, 1000)
+ax.set_ylim(2e-3, 1e2)
+plt.title(r"$\!\tilde{\,D}=0.1$")
+plt.legend(frameon=False, fontsize=13, loc="lower left")
+plt.savefig('low_D.png', dpi=150, bbox_inches='tight')
---
+++ check_perturbation/simulation.py
@@ -0,0 +1,194 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+
+
+max_steps = 400000
+time = [0]
+
+# reaction terms
+C_i = 110
+
+C_f = 111  # initial and final (mean) ligand concentrations
+C0 = np.zeros(max_steps + 1)
+C0[0:int(max_steps/3)] = C_i
+C0[int(max_steps/3):] = C_f
+
+G_i = 1  # initial G-protein concentration
+
+g = 0.05
+phi = np.pi/6
+
+kc = 1  # time in units of 1/kc
+KD = 100
+KG = 1
+alpha = 10
+KM = 0.0001  # Michaelis-Menten constant
+
+# Initializing array
+l = 40  # number of modeled G-protein sites
+n = 400000  # number of receptors
+theta = np.linspace(-np.pi, np.pi, l+1)
+theta = theta[0:l]
+
+# Diffusion terms
+r = 1  # position in units of r
+dt = 0.00005
+dx = 2*np.pi*r/l
+D = 100  # units of x^2/t
+# D*dt/dx**2  # checking stability - this has to be < 0.5
+
+
+#===========================================================
+# High Diffusion
+#===========================================================
+
+print('High diffusion:')
+
+D_list = np.logspace(-4, 2, 10)
+kc = 1
+kc_list = np.logspace(-2, 1, 7)
+D = 50
+alpha = 10
+C_f = KD/alpha - 1
+C_i = KD / np.sqrt(alpha)
+C0 = np.zeros(max_steps + 1)
+
+C_profile = C_f * np.exp(g/2 * np.cos(theta - phi))
+C_list = np.logspace(0, 2.5, 12)
+first_order_corr = []
+first_order_disc = []
+G_end = []
+first_order_prob = []
+p_bound = []
+G_pred_list = []
+K_eff_list = []
+FI_calc = []
+FI_high_pert = []
+f_bound_list = []
+f_bound_fit = []
+
+
+def linear_prob_bound(theta, A, B):
+    return A + B * np.cos(theta-phi)
+
+
+def first_order_high_diff(theta, D):
+    G_opt = KG * (KD - C_f) / (alpha*C_f - KD)
+    G_pert = G_opt - g * np.cos(theta-phi) / (4 * D)
+    return G_pert
+
+
+for j, C_f in enumerate(C_list):
+    C_profile = C_f * np.exp(g/2 * np.cos(theta - phi))
+    G_inact = []
+    G_inact_init = np.zeros(l)
+    for i in range(l):
+        G_inact_init[i] = 50
+    G_inact.append(G_inact_init)
+    G_active_init = KG * (KD - C_i) / (alpha*C_i - KD)
+
+    G_act = G_active_init
+    G_next_act = np.copy(G_active_init)
+    G_inact = G_inact_init
+    G_next_inact = np.copy(G_inact_init)
+
+    for i in range(max_steps):
+        K_eff = KD * (G_next_act + KG)/(alpha*G_next_act + KG)
+
+        # adjusting to the local fraction bound (approx.)
+        f_b = C_profile / (C_profile + K_eff)
+
+        G_next_act = G_act + (dt*D/dx**2)*(np.roll(G_act, 1) - 2*G_act + np.roll(G_act, -1)) + \
+            dt*(kc*(1-f_b)*(G_inact)/(KM + G_inact) -
+                kc*f_b*G_next_act/(KM + G_act))
+        G_act = G_next_act
+
+        G_next_inact = G_inact + (dt*D/dx**2)*(np.roll(G_inact, 1) - 2*G_inact + np.roll(
+            G_inact, -1)) + dt*(-kc*(1-f_b) * (G_inact)/(KM + G_inact) + kc*f_b*G_act/(KM + G_act))
+        G_inact = G_next_inact
+
+    # Calculating Fisher information from probability bound in first order
+    p, cov = opt.curve_fit(linear_prob_bound, theta, f_b)
+    A = p[0]
+    B = p[1]
+    FI_calc.append(n * B**2 / (2 * (A - A**2)))
+
+    G_half = KG * (KD - C_profile) / (alpha*C_profile - KD)
+    G_opt = KG * (KD - C_f) / (alpha*C_f - KD)
+    G_end.append(G_next_act)
+
+    f_bound_list.append(f_b)
+    f_bound_fit.append(linear_prob_bound(theta, A, B))
+    print(f'{j+1}/{len(C_list)}, C0 = {C_f:.4f}')
+
+
+# ===========================================================
+# Low Diffusion
+# ===========================================================
+
+print('\nLow diffusion:')
+
+D = 0.1
+
+first_order_corr = []
+first_order_disc = []
+G_end = []
+first_order_prob = []
+p_bound = []
+FI_calc1 = []
+FI_low_pert = []
+FI_time = []
+
+for j, C_f in enumerate(C_list):
+    C_profile = C_f * np.exp(g/2 * np.cos(theta - phi))
+    G_inact = []
+    G_inact_init = np.zeros(l)
+    for i in range(l):
+        G_inact_init[i] = 50
+    G_inact.append(G_inact_init)
+    G_active_init = KG * (KD - C_i) / (alpha*C_i - KD)
+
+    G_act = G_active_init
+    G_next_act = np.copy(G_active_init)
+    G_inact = G_inact_init
+    G_next_inact = np.copy(G_inact_init)
+    FI_time_j = []
+
+    for i in range(max_steps):
+        K_eff = KD * (G_next_act + KG)/(alpha*G_next_act + KG)
+
+        # adjusting to the local fraction bound (approx.)
+        f_b = C_profile / (C_profile + K_eff)
+
+        G_next_act = G_act + (dt*D/dx**2)*(np.roll(G_act, 1) - 2*G_act + np.roll(G_act, -1)) + \
+            dt*(kc*(1-f_b)*(G_inact)/(KM + G_inact) -
+                kc*f_b*G_next_act/(KM + G_act))
+        G_act = G_next_act
+
+        G_next_inact = G_inact + (dt*D/dx**2)*(np.roll(G_inact, 1) - 2*G_inact + np.roll(
+            G_inact, -1)) + dt*(-kc*(1-f_b) * (G_inact)/(KM + G_inact) + kc*f_b*G_act/(KM + G_act))
+        G_inact = G_next_inact
+
+        p, cov = opt.curve_fit(linear_prob_bound, theta, f_b)
+        E = p[0]
+        F = p[1]
+        FI_time_j.append(n * F**2 / (2 * (E - E**2)))
+
+    # Calculating Fisher information from probability bound in first order
+    p, cov = opt.curve_fit(linear_prob_bound, theta, f_b)
+    A = p[0]
+    B = p[1]
+    FI_calc1.append(n * B**2 / (2 * (A - A**2)))
+
+    G_half = KG * (KD - C_profile) / (alpha*C_profile - KD)
+    G_opt = KG * (KD - C_f) / (alpha*C_f - KD)
+    G_end.append(G_next_act)
+
+    FI_time.append(FI_time_j)
+
+    print(f'{j+1}/{len(C_list)}, C0 = {C_f:.4f}')
+
+
+np.save("FI_calc", np.vstack([FI_calc, FI_calc1]))
+print("\nData saved.")
